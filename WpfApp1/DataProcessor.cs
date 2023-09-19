205251752552b792b4bff196c8a86fd29337ead4
chrgeogit <cgfreecontact@gmail.com>
2023-09-19 23:24:19 +0300

    getjs data

---
+++ WpfApp1/DataProcessor.cs
@@ -0,0 +1,30 @@
+﻿using getData;
+
+namespace WpfApp1
+{
+    public class DataProcessor
+    {
+        private readonly getjsData jsData;
+
+        public DataProcessor(getjsData jsData) 
+        {
+            this.jsData = jsData;
+        }
+        public (double[] labels, double[] values) GetProcessedData() 
+        {
+            try
+            {
+                var labels = jsData.GetLabels();
+                var values = jsData.GetValues();
+
+                return (labels, values);
+
+            }
+            catch
+            {
+                return (null, null);
+            }
+        
+        }
+    }
+}
